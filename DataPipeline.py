d703117d384257904b759e252e3e28733caf4f91
Peter Akioyamen <peter.akioyamen@gmail.com>
2019-10-20 20:48:42 -0400

    Data pre-processing script. Takes images from local disk, downsamples them, and stores them via serialization.

--- DataPipeline.py
+++ DataPipeline.py
@@ -1,15 +1,54 @@
+"""
+@Author : Peter Akioyamen
+@Date : October 20, 2019
+@Code : CAP 4612
+"""
+
 import _pickle as pickle
 import numpy as np
 
 #def helper functions
 
 class DataPipeline :
-    def __init__(self, epochs, batch_size = 32, train_file_path = None, test_file_path = None):
+    """
+    A class which facilitates the functions of a data pipeline for images.
+
+    Attributes
+    ----------
+    batch_size : int
+        The size of the training batches
+    train_path : str
+        The path, including name, designating the location of the training data pickle file
+    test_path : str
+        The path, including name, designating the location of the testing data pickle file
+    train_iterations : int
+        The number of training iterations necessary for one epoch
+
+    Methods
+    -------
+    says(sound=None)
+        Prints the animals name and what sound it makes
+    """
+    def __init__(self, epochs, batch_size = 24, train_file_path = None, test_file_path = None):
+        """
+        The constructor for the DataPipeline class.
+
+        Parameters
+        ----------
+        batch_size : int, optional
+            The size of the training batches (default is 24)
+        train_file_path : str, optional
+            The path, including name, designating the location of the training data pickle file (default is None).
+                if not specified, it is assumed the file is stored in the same directory as the script
+        test_file_path : str, optional
+            The path, including name, designating the location of the testing data pickle file (default is None),
+                if not specified, it is assumed the file is stored in the same directory as the script
+        """
+        # TODO : Decide whether the *_instances attribute is necessary
         self.batch_size = batch_size
-        self.train_instances = int #is this necessary ?
-        self.test_instances = int
         self.train_path = train_file_path
         self.test_path = test_file_path
+
         if train_file_path is None :
             self.train_path = "train_images.pickle"
         if test_file_path is None :
@@ -24,33 +63,83 @@ class DataPipeline :
             print("Please ensure pickle files are in correct directory.\nProgram exit.")
             quit(-1)
 
-        self.iterations = self.train_instances // self.batch_size
-        #self.batch_samples = np.random.randint(low = , high = , )
+        self.train_iterations = self.train_instances // self.batch_size
 
     def get_iterations(self):
+        """
+        Returns the number of training iterations necessary to complete one epoch,
+            this is predetermined by the batch size specified.
+        """
         return self.iterations
 
     def get_training_batch(self) :
+        """ """
+        gray_scale = 1
+        test_features = []
+        test_labels = []
         return None
+
 
     def get_validation_data(self) :
+        # TODO : Decide whether this will be necessary
         return None
 
-    def get_test_data(self) :
-        labels = []
-        features = []
+    def get_test_data(self, test_partition) :
+        """
+        Retrieves the test batch to be evaluated specified by the partition number provided.
 
+        Parameters
+        ----------
+        test_partition : int
+            The partition number which is to be retrieved by this function
+
+        Raises
+        ------
+        Exception
+            If the number of partitions specified as an argument is not within
+                the range of 0 and 5, inclusive
+        """
+
+        # ---
+        gray_scale = 1
+        test_features = []
+        test_labels = []
+
+        # ---
+        if 0 <= test_partition <= 5 :
+            partition = test_partition
+        else :
+            raise Exception("batch_count must be in range [0, 5]")
+
+        # ---
         try :
             with open(self.train_path, "rb", ) as test_data :
                 data_count = pickle.load(test_data)
+                batch_interval = data_count // 6
+
                 for i in range(data_count):
                     instance = pickle.load(test_data)
-                    labels.append(instance[0])
-                    features.append(np.asarray(instance[1]))
+                    if (
+                            (partition == 0 and i < batch_interval) or (
+                            partition == 1 and batch_interval <= i < batch_interval * 2) or (
+                            partition == 2 and batch_interval * 2 <= i < batch_interval * 3) or (
+                            partition == 3 and batch_interval * 3 <= i < batch_interval * 4) or (
+                            partition == 4 and batch_interval * 4 <= i < batch_interval * 5) or (
+                            partition == 5 and i >= batch_interval * 5)
+                    ) :
+                        test_labels.append(instance[0])
+                        test_features.append(np.asarray(instance[1]))
         except OSError as err :
             print(err.strerror)
             print("Please ensure pickle files are in correct directory.\nProgram exit.")
             quit(-1)
 
-        labels = np.asarray(labels)
-        features = np.asarray(features)
+        # ---
+        test_features = np.asarray(test_features)
+        test_labels = np.asarray(test_labels)
+        shape = test_features.shape
+        test_features /= 255.0
+        test_features = np.reshape(test_features, (shape[0], shape[1], shape[2], gray_scale))
+
+        # ---
+        return test_features, test_labels
--- PictureProcessor.py
+++ PictureProcessor.py
@@ -5,44 +5,60 @@
 
 This python script is a part of the early stage data pipeline.
 The data are chest XRay images of varying dimensionality. This
-script reads images from local disk, downsamples them, and serializes
+script reads images from local disk, down-samples them, and serializes
 the images and their labels for storage.
 """
 
-# Import necessary libraries
-from PIL import Image
-import _pickle as pickle
-import glob
-import os
 
-train_normal_path = r"C:\Users\Peter\Documents\UWO Work\Year 3\Sem1\CS Machine Learning (POLY)\Project Marrow\DataFiles\chest_xray\train\NORMAL"
-train_pneumonia_path = r"C:\Users\Peter\Documents\UWO Work\Year 3\Sem1\CS Machine Learning (POLY)\Project Marrow\DataFiles\chest_xray\train\PNEUMONIA"
-test_normal_path = r"C:\Users\Peter\Documents\UWO Work\Year 3\Sem1\CS Machine Learning (POLY)\Project Marrow\DataFiles\chest_xray\test\NORMAL"
-test_pneumonia_path = r"C:\Users\Peter\Documents\UWO Work\Year 3\Sem1\CS Machine Learning (POLY)\Project Marrow\DataFiles\chest_xray\test\PNEUMONIA"
-image_paths = {"train_normal": os.path.join(train_normal_path, "*.jpeg"),
-               "train_pneumonia": os.path.join(train_pneumonia_path, "*.jpeg"),
-               "test_normal": os.path.join(test_normal_path, "*.jpeg"),
-               "test_pneumonia": os.path.join(test_pneumonia_path, "*.jpeg")}
-image_files = {key: glob.glob(path) for key, path in image_paths.items()}
-image_files_keys = list(image_files.keys())
-count_images = [sum([len(image_files[image_files_keys[0]]), len(image_files[image_files_keys[1]])]),
-                sum([len(image_files[image_files_keys[2]]), len(image_files[image_files_keys[3]])])]
-dim = [750, 750]
+# Define main function of script
+def main() :
+    # Import necessary libraries
+    from PIL import Image
+    import _pickle as pickle
+    import glob
+    import os
 
-with open("train_images.pickle", "ab+") as train_f, open("test_images.pickle", "ab+") as test_f :
-    pickle.dump(count_images[0], train_f)
-    pickle.dump(count_images[1], test_f)
-    for key, image_category in image_files.items() :
-        for image in image_category :
-            with Image.open(image) as im :
-                im = im.resize(dim)
-                if key == image_files_keys[0] :
-                    pickle.dump([1, im], train_f)
-                elif key == image_files_keys[1] :
-                    pickle.dump([0, im], train_f)
-                elif key == image_files_keys[2] :
-                    pickle.dump([1, im], test_f)
-                else :
-                    pickle.dump([0, im], test_f)
-            im = None
-    print("Success")
+    # Define local file paths to image directories
+    train_normal_path = r"C:\Users\Peter\Documents\UWO Work\Year 3\Sem1\CS Machine Learning (POLY)\Project Marrow\DataFiles\chest_xray\train\NORMAL"
+    train_pneumonia_path = r"C:\Users\Peter\Documents\UWO Work\Year 3\Sem1\CS Machine Learning (POLY)\Project Marrow\DataFiles\chest_xray\train\PNEUMONIA"
+    test_normal_path = r"C:\Users\Peter\Documents\UWO Work\Year 3\Sem1\CS Machine Learning (POLY)\Project Marrow\DataFiles\chest_xray\test\NORMAL"
+    test_pneumonia_path = r"C:\Users\Peter\Documents\UWO Work\Year 3\Sem1\CS Machine Learning (POLY)\Project Marrow\DataFiles\chest_xray\test\PNEUMONIA"
+    image_paths = {"train_normal": os.path.join(train_normal_path, "*.jpeg"),
+                   "train_pneumonia": os.path.join(train_pneumonia_path, "*.jpeg"),
+                   "test_normal": os.path.join(test_normal_path, "*.jpeg"),
+                   "test_pneumonia": os.path.join(test_pneumonia_path, "*.jpeg")}
+
+    # Retrieve the full paths of the images including file names and extensions
+    # Get the number of images and define the image resizing dimensions
+    image_files = {key: glob.glob(path) for key, path in image_paths.items()}
+    image_files_keys = list(image_files.keys())
+    count_images = [sum([len(image_files[image_files_keys[0]]), len(image_files[image_files_keys[1]])]),
+                    sum([len(image_files[image_files_keys[2]]), len(image_files[image_files_keys[3]])])]
+    dim = [750, 750]
+
+    # Save the number of test and training images
+    # Open each image file, resize it, encode into binary and save it to a pickle file
+    # Generate and save the labels of the images
+    with open("train_images.pickle", "ab+") as train_f, open("test_images.pickle", "ab+") as test_f :
+        pickle.dump(count_images[0], train_f)
+        pickle.dump(count_images[1], test_f)
+        for key, image_category in image_files.items() :
+            for image in image_category :
+                with Image.open(image) as im :
+                    im = im.resize(dim)
+                    if key == image_files_keys[0] :
+                        pickle.dump([1, im], train_f)
+                    elif key == image_files_keys[1] :
+                        pickle.dump([0, im], train_f)
+                    elif key == image_files_keys[2] :
+                        pickle.dump([1, im], test_f)
+                    else :
+                        pickle.dump([0, im], test_f)
+                im = None
+
+        # Notify user of successful execution
+        print("Success")
+
+
+if __name__ == '__main__':
+    main()
