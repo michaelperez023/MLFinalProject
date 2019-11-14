6bd77042d3f19df365fa174d0b3a7b8e76845207
Peter Akioyamen <peter.akioyamen@gmail.com>
2019-11-13 22:30:12 -0500

    All trained machine learning models

--- ML_Models/NBClassifier.py
+++ ML_Models/NBClassifier.py
@@ -1,20 +1,29 @@
+"""
+@Author : Jeffrey Krakowiak
+@Author : Peter Akioyamen
+@Code : CAP 4612
+
+This python script is a part of the model development process.
+It acts to instantiate and train 4 Multinomial Naive Bayes
+classification models which use mini-batch training.
+"""
+
 from sklearn.naive_bayes import MultinomialNB
 from ML_DataPipeline import DataPipeline
 import _pickle as pickle
-import joblib
 import os
 
 
-path = r"C:\Users\jeffk\Desktop\MLFinalProject-master"
+path = r"C:\Users\Peter\Documents\UWO Work\Year 3\Sem1\CS Machine Learning (POLY)\Project Marrow"
 train_path = os.path.join(path, "train_images.pickle")
 test_path = os.path.join(path, "test_images.pickle")
-epochs = 10 # varied from 10-30 by 10
+epochs = 1 # varied from 1, 10-30 by 10
 class_flag = True
 labels = [0, 1]
 
 
 pipeline = DataPipeline("NB", batch_size=24, train_file_path=train_path, test_file_path=test_path)
-nb =MultinomialNB(alpha=1.0)
+nb = MultinomialNB()
 
 for epoch in range(epochs):
     if (epoch % 2) == 0:
@@ -30,6 +39,6 @@ for epoch in range(epochs):
             continue
         nb.partial_fit(x_train, y_train)
 
-with open("MultinomialNB_" + str(epochs) + "_.pickle", "ab") as model_file:
+with open("NBClassifier_" + str(epochs) + "_.pickle", "ab") as model_file:
     pickle.dump(nb, model_file)
 
--- ML_Models/SGDClassifier.py
+++ ML_Models/SGDClassifier.py
@@ -1,3 +1,13 @@
+"""
+@Author : Peter Akioyamen
+@Code : CAP 4612
+
+This python script is a part of the model development process.
+It acts to instantiate and train 4 Support Vector Machine
+classification models which use Gradient Descent as the
+learning algorithm, enabling mini-batch training.
+"""
+
 from sklearn.linear_model import SGDClassifier
 from ML_DataPipeline import DataPipeline
 import _pickle as pickle
@@ -8,7 +18,7 @@ import os
 path = r"C:\Users\Peter\Documents\UWO Work\Year 3\Sem1\CS Machine Learning (POLY)\Project Marrow"
 train_path = os.path.join(path, "train_images.pickle")
 test_path = os.path.join(path, "test_images.pickle")
-epochs = 30 # varied from 10-30 by 10
+epochs = 1 # varied from 1, 10-30 by 10
 class_flag = True
 labels = [0, 1]
 seed = 42
